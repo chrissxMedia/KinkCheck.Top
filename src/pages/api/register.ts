0a5f908f098202961408ec5f6d2374af0d907a50
pixel <pixel+github@chrissx.de>
2024-01-03 07:25:09 +0100

    api/register: set username already

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -4,14 +4,15 @@ import { supabase } from "../../base";
 export const POST: APIRoute = async ({ request, redirect }) => {
     const formData = await request.formData();
     const email = formData.get("email")?.toString();
+    const username = formData.get("username")?.toString();
     const password = formData.get("password")?.toString();
     const captchaToken = formData.get("h-captcha-response")?.toString();
 
-    if (!email || !password || !captchaToken) {
-        return new Response(`No ${!email ? "email" : !password ? "password" : "captcha token"} given`, { status: 400 });
+    if (!email || !username || !password || !captchaToken) {
+        return new Response(`No ${!email ? "email" : !username ? "username" : !password ? "password" : "captcha token"} given`, { status: 400 });
     }
 
-    const { error } = await supabase.auth.signUp({ email, password, options: { captchaToken } });
+    const { error } = await supabase.auth.signUp({ email, password, options: { captchaToken, data: { username } } });
 
     if (error) {
         return new Response(error.message, { status: 500 });
