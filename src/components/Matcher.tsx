aa75638983ed4ba4b61bd0e4f95880011b02ac63
pixel <pixel+github@chrissx.de>
2024-01-11 15:08:15 +0100

    split `base` and `backendlib`, implement more for...template storage mainly
    also includes test improvements, adjustments to everything affected and
    proper caching using `node-cache`
    
    this is unfortunately a quite large commit, just because of how
    interwoven these changes are

--- src/components/Matcher.tsx
+++ src/components/Matcher.tsx
@@ -1,5 +1,5 @@
 import { useState } from "preact/hooks";
-import { decodeKinkCheck, defaultKinkcheck, kinks, ratings, type metadata } from "../base";
+import { decodeKinkCheck, defaultKinkcheck, type ratings, type template_revision } from "../base";
 import { Category } from "./KinkCheck";
 import kc from "./KinkCheck.module.css";
 import styles from "./Matcher.module.css";
@@ -11,21 +11,24 @@ function matchRating(a: number, b: number): number {
 }
 
 export function match(a: ratings, b: ratings): ratings {
+    // TODO: better compat check
     if (!Object.keys(a).every((k) => Object.keys(b).includes(k))) throw "incompatible ratings";
-    return Object.fromEntries(Object.entries(a).map(
-        ([cat, rA]) => [cat, rA.map((rsA, kink) => rsA.map((ratA, pos) => matchRating(ratA, b[cat][kink][pos])))]
-    ));
+    // FIXME: we need proper typing
+    return a.map(
+        (rA, cat) => rA.map(
+            (rsA, kink) => rsA.map(
+                (ratA, pos) => matchRating(ratA, b[cat][kink][pos]))));
 }
 
-export default function Matcher(meta: metadata) {
+export default function Matcher(meta: template_revision) {
     const [partnerA, setPartnerA] = useState("");
     const [partnerB, setPartnerB] = useState("");
-    let kcA = defaultKinkcheck(kinks);
-    let kcB = defaultKinkcheck(kinks);
-    let errorA, errorB;
-    try { kcA = decodeKinkCheck(meta, partnerA.trim()); } catch(e: any) { errorA = e.toString(); }
-    try { kcB = decodeKinkCheck(meta, partnerB.trim()); } catch(e: any) { errorB = e.toString(); }
-    kcB.ratings = Object.fromEntries(Object.entries(kcB.ratings).map(([k, r]) => [k, r.map(rs => rs.toReversed())]));
+    let kcA = defaultKinkcheck(meta.kinks);
+    let kcB = defaultKinkcheck(meta.kinks);
+    let errorA: string | undefined, errorB: string | undefined;
+    try { kcA = decodeKinkCheck(meta, partnerA.trim()); } catch (e: any) { errorA = e.toString(); }
+    try { kcB = decodeKinkCheck(meta, partnerB.trim()); } catch (e: any) { errorB = e.toString(); }
+    kcB.ratings = kcB.ratings.map((r) => r.map(rs => rs.toReversed()));
     const matched = match(kcA.ratings, kcB.ratings);
     return <main>
         <div class={styles.matcherfrontmatter + " " + kc.catcontainer}>
@@ -35,8 +38,8 @@ export default function Matcher(meta: metadata) {
         </div>
         <div class={kc.catcontainer}>
             {
-                Object.entries(kinks).map(([cat, kinks]) => (
-                    <Category cat={cat} kinks={kinks} ratings={matched[cat]} />
+                meta.kinks.map(([cat, kinks], c) => (
+                    <Category cat={cat} kinks={kinks} ratings={matched[c]} />
                 ))
             }
         </div>
