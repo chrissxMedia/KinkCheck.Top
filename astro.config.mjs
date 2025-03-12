3305ac6204ad7834350318a99e6a983e6387e6b3
pixel <pixel+github@chrissx.de>
2025-03-12 16:54:14 +0100

    add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 import db from "@astrojs/db";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), db()]
+  integrations: [preact(), db()],
+
+  adapter: node({
+    mode: "standalone"
+  }),
 });
