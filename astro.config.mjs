23d2360ed2667cb540831396f36b9c2a09a0b400
pixel <pixel+github@chrissx.de>
2025-05-02 18:48:15 +0200

    install svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import db from "@astrojs/db";
 import node from "@astrojs/node";
+import svelte, { vitePreprocess } from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), db()],
+  integrations: [preact(), db(), svelte({ preprocess: vitePreprocess() })],
 
   adapter: node({
     mode: "standalone"
