c4a05d805eff33235015840ad3381e67df64bc63
pixel <pixel+github@chrissx.de>
2026-07-06 01:56:50 +0200

    drizzle-kit: remove dbCredentials
    it's not needed for migration generation, which is the only thing we'll ever use it for

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "drizzle-kit";
-import { dbFile } from "./src/db/config";
 
 export default defineConfig({
   out: "migrations",
   schema: "src/db/config.ts",
   dialect: "sqlite",
-  dbCredentials: { url: dbFile },
 });
