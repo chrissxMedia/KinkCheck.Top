254c81764462d667ebb04daf4cd7450b878b4b2f
pixel <pixel+github@chrissx.de>
2026-07-09 01:33:11 +0200

    add astro action: check.save
    including a bunch of validation .. that is realistically speaking unnecessary but whatever

--- test/checks.test.ts
+++ test/checks.test.ts
@@ -1,5 +1,7 @@
 import { expect, test } from "vitest";
 import { decodeKinkCheck, encodeKinkCheck, type template_revision } from "../src/base";
+import type { checkData } from "../src/db/config";
+import { sanitizeCheck } from "../src/actions";
 
 const exampleMeta1: template_revision = {
     revision: "0.1",
@@ -31,8 +33,8 @@ const exampleMeta2: template_revision = {
     ],
 };
 
-const exampleCheck = { ratings: [[1, 2], [3, 4], [1.5]] };
-const exampleCheck2 = { ratings: [[1, 2], [3, 4], [1.5, 1.5]] };
+const exampleCheck: checkData = { ratings: [[1, 2], [3, 4], [1.5]] };
+const exampleCheck2: checkData = { ratings: [[1, 2], [3, 4], [1.5, 1.5]] };
 
 test("re-encoding", () => {
     expect(encodeKinkCheck(exampleMeta1, decodeKinkCheck(exampleMeta1, exampleCheck)))
@@ -40,7 +42,7 @@ test("re-encoding", () => {
 });
 
 test("extra data is ignored", () => {
-    expect(decodeKinkCheck(exampleMeta1, { extra: "data", ...exampleCheck }))
+    expect(decodeKinkCheck(exampleMeta1, { extra: "data", ...exampleCheck } as checkData))
         .toStrictEqual(decodeKinkCheck(exampleMeta1, exampleCheck));
 });
 
@@ -52,4 +54,16 @@ test("extra kinks are ignored", () => {
 test("moving kinks around and slightly adjusting them results in the same encoding", () => {
     expect(encodeKinkCheck(exampleMeta2, decodeKinkCheck(exampleMeta2, exampleCheck)))
         .toStrictEqual(exampleCheck2);
+});
+
+test("valid input passes", () => {
+    expect(sanitizeCheck(exampleMeta1, exampleCheck)).toStrictEqual(exampleCheck);
+});
+
+test("empty ratings array", () => {
+    expect(() => sanitizeCheck(exampleMeta1, { ratings: [] })).not.toThrow();
+});
+
+test("incorrect number of positions", () => {
+    expect(() => sanitizeCheck(exampleMeta1, { ratings: [[0]] })).toThrow();
 });
