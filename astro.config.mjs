753e18baf72e4e53c830770c6633317487a3601e
pixel <pixel+github@chrissx.de>
2026-07-11 06:05:40 +0200

    limit body size to 1 MiB

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  adapter: node({ mode: "standalone" }),
+  adapter: node({ mode: "standalone", bodySizeLimit: 1024 * 1024 /* 1 MiB is plenty for now */ }),
   env: {
     schema: {
       KCT_DATABASE_FILE: envField.string({ context: "server", access: "public", default: "./.dev.db" }),
