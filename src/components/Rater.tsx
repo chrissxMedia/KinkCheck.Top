4c74db69699942d2cb346b90e1cce97b0acb6b0c
pixel <pixel+github@chrissx.de>
2026-08-01 20:13:30 +0200

    Rater: extract updateRating out of handleClick

--- src/components/Rater.tsx
+++ src/components/Rater.tsx
@@ -8,16 +8,19 @@ function background(rating: number): string {
 
 export default function Rater({ text, rating, setRating }:
     { text: string, rating: number, setRating?: (r: number) => void }) {
-    const handleClick = !setRating ? undefined : (e: MouseEvent) => {
-        e.preventDefault();
-        const step = e.shiftKey || e.altKey ? 0.5 : 1;
-        let newRating = e.button ? rating - step : rating + step;
+    const updateRating = setRating && ((x: number) => {
+        let newRating = rating + x;
         const max = ratings.length - 1;
-        if (newRating === 0.5) newRating = e.button ? 0 : 1;
+        if (newRating === 0.5) newRating = x > 0 ? 1 : 0;
         else if (newRating > max) newRating = 0;
         else if (newRating < 0) newRating = max;
         setRating(newRating);
-    };
+    });
+    const handleClick = updateRating && ((e: MouseEvent) => {
+        e.preventDefault();
+        const step = e.shiftKey || e.altKey ? 0.5 : 1;
+        updateRating(e.button ? -step : +step);
+    });
     return (
         <div class={setRating ? styles.clickable : styles.noclick}
             onClick={handleClick} onContextMenu={handleClick}>
